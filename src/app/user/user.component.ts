3f4b46d21c9e3591b39ddfd546c37f0b5abd34c4
Chanh Duc Ngo <chanhduc@gmail.com>
2024-06-26 20:48:45 +0200

    Add UserComponent

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../../dummy-users';
 
+
+const randomIndex = Math.random() * DUMMY_USERS.length | 0;
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -9,4 +12,14 @@ import { Component } from '@angular/core';
 })
 export class UserComponent {
 
+  selectedUser = DUMMY_USERS[randomIndex];
+
+  get avatarPath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+
+  onSelectUser() {
+    const newRandomIndex = Math.random() * DUMMY_USERS.length | 0;
+    this.selectedUser = DUMMY_USERS[newRandomIndex];
+    }
 }
